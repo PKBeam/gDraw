54487074e128aaee539b7b19dbafee67e5f08ef1
PKBeam <pkchu99@gmail.com>
2018-09-24 09:59:14 +1000

    Initial Commit

---
+++ gDraw/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  gDraw
+//
+//  Created by Vincent Liu on 24/9/18.
+//  Copyright © 2018 Vincent Liu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
